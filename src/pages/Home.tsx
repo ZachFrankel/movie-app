7d111ca2fd096350f79a375d407bd5faa6eaf60f
ZachF <zfrankel2016@gmail.com>
2025-03-15 14:42:21 +0000

    Implemented a search component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,24 @@
+import { WideContainer } from "../components/layout/WideContainer";
+import Search from "../components/features/Search";
+
 function Home() {
   return (
     <>
       <div className="relative z-10 mb-16 sm:mb-24">
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8 max-w-screen-md">
-        <div className="mt-44 space-y-16 text-center">
-          <div className="absolute bottom-0 left-0 right-0 flex h-0 justify-center">
-            <div className="absolute bottom-4 h-[100vh] w-[3000px] rounded-[100%] bg-denim-300 md:w-[200vw]"></div>
+        <div className="container mx-auto px-4 sm:px-6 lg:px-8 max-w-screen-md">
+          <div className="mt-44 space-y-16 text-center">
+            <div className="absolute bottom-0 left-0 right-0 flex h-0 justify-center">
+              <div className="absolute -bottom-12 h-[100vh] w-[3000px] rounded-[100%] bg-denim-300 md:w-[200vw]"></div>
+            </div>
+            <WideContainer>
+              <Search
+                title="What do you want to watch?"
+                placeholder="What do you want to watch?"
+              />
+            </WideContainer>
           </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
