a98641c5412845b19867fa39843b1b66834bb119
ZachF <zfrankel2016@gmail.com>
2025-03-15 14:14:29 +0000

    Added curved backdrop on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,15 @@
 function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <div className="relative z-10 mb-16 sm:mb-24">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8 max-w-screen-md">
+        <div className="mt-44 space-y-16 text-center">
+          <div className="absolute bottom-0 left-0 right-0 flex h-0 justify-center">
+            <div className="absolute bottom-4 h-[100vh] w-[3000px] rounded-[100%] bg-denim-300 md:w-[200vw]"></div>
+          </div>
+        </div>
+      </div>
+    </div>
     </>
   );
 }
