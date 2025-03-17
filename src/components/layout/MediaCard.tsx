6ed67a37fb8543a0b15790a91cd455f9e766312e
ZachF <zfrankel2016@gmail.com>
2025-03-17 12:39:21 +0000

    Updated hover effect for the MediaCard

--- src/components/layout/MediaCard.tsx
+++ src/components/layout/MediaCard.tsx
@@ -12,7 +12,7 @@ export function MediaCard({ posterPath, title, mediaType, year }: MediaCardProps
   
   return (
     <div className="group rounded-xl bg-background-main bg-opacity-0 transition-colors duration-100 hover:bg-opacity-100">
-      <article className="pointer-events-auto relative mb-2 p-[0.4em] transition-transform duration-300 w-full">
+      <article className="pointer-events-auto relative mb-2 p-[0.4em] transition-transform duration-300 w-full group-hover:scale-95">
         <div className="relative mb-4 aspect-[2/3] w-full overflow-hidden rounded-xl bg-denim-500 bg-cover bg-center transition-[border-radius] duration-100 group-hover:rounded-lg">
           <img
             src={imageUrl}
