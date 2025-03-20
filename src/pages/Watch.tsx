5586458e3e3c623804f02b320afa7b34d8457b98
ZachF <23frankelz654@collyers.ac.uk>
2025-03-20 13:35:34 +0000

    Started on TV functionality

--- src/pages/Watch.tsx
+++ src/pages/Watch.tsx
@@ -3,6 +3,15 @@ import { useParams } from "react-router-dom";
 import { VideoPlayer } from "../components/player/Player";
 import { VideoSource } from "../components/player/types";
 
+async function getTitle(id: string, type: string) {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/${type}/${id}?api_key=${import.meta.env.VITE_TMDB_API_KEY}&language=en-US`
+  );
+  
+  const data = await response.json();
+  return data.title || data.name;
+}
+
 export function WatchMovie() {
   const { id } = useParams();
   const [title, setTitle] = useState('');
@@ -10,16 +19,12 @@ export function WatchMovie() {
   useEffect(() => {
     if (!id) return;
     
-    const fetchMovieTitle = async () => {
-      const response = await fetch(
-        `https://api.themoviedb.org/3/movie/${id}?api_key=${import.meta.env.VITE_TMDB_API_KEY}&language=en-US`
-      );
-      
-      const data = await response.json();
-      setTitle(data.title);
+    const fetchTitle = async () => {
+      const title = await getTitle(id, 'movie');
+      setTitle(title);
     };
 
-    fetchMovieTitle();
+    fetchTitle();
   }, [id]);
 
   const sources: VideoSource[] = [
@@ -42,11 +47,32 @@ export function WatchMovie() {
 export function WatchTV() {
   const { id, season, episode } = useParams();
 
+  const [title, setTitle] = useState('');
+
+  useEffect(() => {
+    if (!id) return;
+    
+    const fetchTitle = async () => {
+      const title = await getTitle(id, 'tv');
+      setTitle(title);
+    };
+
+    fetchTitle();
+  }, [id]);
+
+  const sources: VideoSource[] = [
+    {
+      src: `https://files.vidstack.io/sprite-fight/hls/stream.m3u8`,
+      type: 'hls'
+    }
+  ];
+
   return (
     <>
-      <p>{id}</p>
-      <p>{season}</p>
-      <p>{episode}</p>
+      <VideoPlayer 
+        sources={sources}
+        title={title}
+      />
     </>
   );
 }
