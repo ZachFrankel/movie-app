582e270fd2515577e7539238bb2100f67aeecd48
ZachF <23frankelz654@collyers.ac.uk>
2025-03-20 13:19:07 +0000

    Uses the title of the movie instead of id

--- src/pages/Watch.tsx
+++ src/pages/Watch.tsx
@@ -1,9 +1,26 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { VideoPlayer } from "../components/player/Player";
 import { VideoSource } from "../components/player/types";
 
 export function WatchMovie() {
   const { id } = useParams();
+  const [title, setTitle] = useState('');
+
+  useEffect(() => {
+    if (!id) return;
+    
+    const fetchMovieTitle = async () => {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/movie/${id}?api_key=${import.meta.env.VITE_TMDB_API_KEY}&language=en-US`
+      );
+      
+      const data = await response.json();
+      setTitle(data.title);
+    };
+
+    fetchMovieTitle();
+  }, [id]);
 
   const sources: VideoSource[] = [
     {
@@ -16,7 +33,7 @@ export function WatchMovie() {
     <>
       <VideoPlayer 
         sources={sources}
-        title={id}
+        title={title}
       />
     </>
   );
